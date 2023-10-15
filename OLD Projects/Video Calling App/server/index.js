c29a041448028f9b34df209466bb5ae813fa1aa9
Rio <126503340+its-rio@users.noreply.github.com>
2023-10-15 12:36:37 +0530

    Docs: Old files
    Old react projects that create in 2nd sem. but didn't learn anything

---
+++ OLD Projects/Video Calling App/server/index.js
@@ -0,0 +1,21 @@
+const { emit } = require("nodemon");
+const {Server, Socket} = require("socket.io");
+
+const io = new Server(8000, {
+    cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+    console.log('Socket Connected', socket.id);
+    socket.on("room:join", (data) => {
+        const {email, room} = data;
+        emailToSocketIdMap.set(email, socket.id);
+        socketidToEmailMap.set(socket.id, email);
+        io.to(room).emit("room:joined", {email, id: socket.id});
+        socket.join(room);
+        io.to(socket.id).emit("room:join",data);
+    });
+}); 
