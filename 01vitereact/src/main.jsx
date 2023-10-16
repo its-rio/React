fc3a78cf8a86545381548e6b7bbbc4f6dad4d8e9
Rio <126503340+its-rio@users.noreply.github.com>
2023-10-16 21:16:37 +0530

    feat: evaluated expression
    run a function in main.jsx, and variable creation & call. and evaluated expression.

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Chai from './chai.jsx'
 
+function Sam(){
+  let username = "Sangu";
 
+  return(
+    <>
+      <h1>Hello Brother {username}</h1>
+    </>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Chai/>
+    {/* // Running function with evaluated expression */}
+    <Sam/>
   </React.StrictMode>
 )
