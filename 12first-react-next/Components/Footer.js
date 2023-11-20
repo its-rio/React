1db38634578a719c624443c1d6fb65c492993c64
Rio <126503340+its-rio@users.noreply.github.com>
2023-11-21 00:42:40 +0530

    axios

--- 12first-react-next/Components/Footer.js
+++ 12first-react-next/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = (props) => {
   return (
-    <div className='bg-pink-400 h-20 mt-80'>Footer
+    <div className='bg-pink-400 h-40 mt-15'>Footer
         <h1>Editor is {props.edit}</h1>
     </div>
   )
--- 12first-react-next/app/page.js
+++ 12first-react-next/app/page.js
@@ -4,11 +4,31 @@ import React,{useState} from 'react'
 
 
 const page = () => {
+  const [images, setImages] = useState([])
+  let getImages = async () => {
+    try {
+      const response = await axios.get("https://picsum.photos/v2/list")
+      const data = response.data
+      setImages(data);
+      // console.log(images)
+    } catch (error) {
+      console.error("Error aa gaya")
+    }
+    
+  }
   return (
     <>
     <div> 
       <h1>Main Page</h1> <br/>
-      <button className='px-5 py-3 bg-green-700 font-bold rounded-xl m-5'>Get Images </button>
+      <button onClick={getImages} className='px-5 py-3 bg-green-700 font-bold rounded-xl m-5 text-white'>Get Images </button>
+      <div className='p-10'>
+          {images.map((elem, i)=>{
+            return <img key={i} 
+            src={elem.download_url}
+            width={300} height={300} className='rounded-xl m-10 inline-block'
+            />
+          })}
+      </div>
     </div>
 
     </>
